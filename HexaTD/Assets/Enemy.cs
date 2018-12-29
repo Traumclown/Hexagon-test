e15a55561867d7f4f258dd056dbd065afa3c2954
Dennis <dennis@schistek.de>
2018-12-29 21:38:09 +0100

    test

---
+++ HexaTD/Assets/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+    private Transform target;
+    private int wavepointindex = 0;
+
+	// Use this for initialization
+	void Start () {
+        target = Waypoints.wayPoints[0];
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime,Space.World);
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+
+        }
+    }
+    void GetNextWaypoint()
+    {
+        if (wavepointindex>=Waypoints.wayPoints.Length-1)
+        {
+            Destroy(gameObject);
+
+        }
+        wavepointindex++;
+        target = Waypoints.wayPoints[wavepointindex];
+
+
+    }
+}
---
+++ HexaTD/Assets/Waypoints.cs
@@ -0,0 +1,26 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+
+
+    public static Transform[] wayPoints;
+    public void Awake()
+    {
+        //Debug
+        wayPoints = new Transform[transform.childCount];
+        for (int i = 0; i < wayPoints.Length; i++)
+        {
+            wayPoints[i] = transform.GetChild(i);
+           // if (wayPoints[i].GetComponent<MeshFilter>().mesh!=null)
+            {
+         //       Debug.Log(wayPoints[i].GetComponent<MeshRenderer>().material.name);
+            }
+
+        }
+        Debug.Log("Hello" + wayPoints.Length);
+
+    }
+
+}
