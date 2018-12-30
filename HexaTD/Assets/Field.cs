1f3a0a356d7ed11ca5b117387d1599f02b6cabf3
MiauzGenau <46249106+MiauzGenau@users.noreply.github.com>
2018-12-30 15:53:29 +0100

    test

---
+++ HexaTD/Assets/Field.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Field : MonoBehaviour
+{
+
+    public static bool path = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(gameObject.GetComponent<Renderer>().material.name != "")
+        {
+            Debug.Log("not empty");
+            Debug.Log(gameObject.GetComponent<Renderer>().material.name);
+            if(gameObject.GetComponent<Renderer>().material.name != "path")
+            {
+                Debug.Log("hit");
+                path = true;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
